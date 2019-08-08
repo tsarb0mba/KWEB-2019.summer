741b61d7adf707abe0dce60cbecda02c45ee74ba
chenzhige <tsar3701@korea.ac.kr>
2019-08-08 22:53:04 +0800

    방명록 등록하는 것 구현

--- KWEB_07.29_script.js
+++ KWEB_07.29_script.js
@@ -14,7 +14,23 @@ function check_memo_length(){
     document.getElementById("Guest-memo-input").innerHTML=memo.length;
 }
 
+function upload_memo(){
+    let text=document.getElementById("Guest-memo-text").value;
+    let memo_list=document.getElementById("Guest-memo-list");
+    if(text.length>400){
+        alert("your input exceed 400 point.");
+    }
+    else{
+        memo_list.innerHTML+="<div class='Guest-memo-list-element'>"+text+"</div>";
+    }
+
+    document.getElementById("Guest-memo-text").innerHTML="";
+    
+}
+
 let guest_memo = document.getElementById("Guest-memo");
+let button_click=document.getElementById("Guest-memo-button");
 
 document.addEventListener('scroll', () => change_menubar_position());
 guest_memo.addEventListener('keyup',() => check_memo_length());
+button_click.addEventListener("click",() => upload_memo());
